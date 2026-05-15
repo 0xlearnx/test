fc7a3efab47989e96e2184c010806abc7d7d1e49
0xlearnx <notkratoz@duck.com>
2026-05-15 21:30:46 +0530

    Implemented the required Arithmetic Fucntion And Removed unwanted files

--- wrapper.c
+++ wrapper.c
@@ -16,23 +16,42 @@ if (argc != 4){
 int num1=atoi(argv[2]); //converting argv_2 to integer
 int num2=atoi(argv[3]); //converting argv_3 to integer	
 
+// Arithmetic Functions
+
+int add(int a,int b){
+    return a+b;
+}
+
+int multiply(int a,int b){
+    return a*b;
+}
+
+int divsion(int a,int b){
+    return a/b;
+}
+
+int sub(int a,int b){
+    return a-b;
+}
+
+
 // Checking and calling respective functions 
 
 if(strcmp(argv[1],"addition")==0){ 
 
-  printf("Result:%d\n",add(num1,num2));
+  printf("Result: %d\n",add(num1,num2));
 
 }else if((strcmp(argv[1],"subraction")==0){
 
-  printf("\nResult:%d\n",sub(num1,num2));
+  printf("\nResult: %d\n",sub(num1,num2));
 
 }else if((strcmp(argv[1],"multipy")==0){
 
-  printf("\nResult:%d\n",multiply(num1,num2));
+  printf("\nResult: %d\n",multiply(num1,num2));
 
 }else if((strcmp(argv[1],"divsion")==0){
 
-  printf("\nResult:%d\n",divsion(num1,num2));
+  printf("\nResult: %d\n",divsion(num1,num2));
 
 }else {
 
