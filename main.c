2dc167ac84946ae98daf06d31ead16b1d031b78a
0xlearnx <notkratoz@duck.com>
2026-05-15 21:16:24 +0530

    Implemented the functions required for the calculator

--- main.c
+++ main.c
@@ -4,16 +4,50 @@ int add(int a,int b){
     return a+b;
 }
 
+int multi(int a,int b){
+    return a*b;
+}
+
+int div(int a,int b){
+    return a/b;
+}
+
 int sub(int a,int b){
     return a-b;
 }
 
-// n1 ,n2 is removed 
+int n1,n2;
 
 void main(){
+    // Add
+    printf("====================\n\tAdd\n===================\n");
     printf("Number 1 : ");
     scanf("%d",&n1);
-    printf("\nNumber 2 : ");
+    printf("Number 2 : ");
     scanf("%d",&n2);
-    printf("Result : %d",add(n1,n2));
+    printf("\nResult : %d",add(n1,n2));    
+
+    // Subtract
+    printf("\n====================\n\tSubtract\n====================\n");
+    printf("Number 1 : ");
+    scanf("%d",&n1);
+    printf("Number 2 : ");
+    scanf("%d",&n2);
+    printf("\nResult : %d",sub(n1,n2));
+
+    // Multiply
+    printf("\n====================\n\tMultiply\n====================\n");
+    printf("Number 1 : ");
+    scanf("%d",&n1);
+    printf("Number 2 : ");
+    scanf("%d",&n2);
+    printf("\nResult : %d",multi(n1,n2));
+
+    // Divide
+    printf("\n====================\n\tDivide\n====================\n");
+    printf("Number 1 : ");
+    scanf("%d",&n1);
+    printf("Number 2 : ");
+    scanf("%d",&n2);
+    printf("\nResult : %d",div(n1,n2));
 }
